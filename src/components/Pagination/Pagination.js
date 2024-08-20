7006cb6ec35fcc5f2a616aca1dec78e2c5dc1daa
rakgoorak <rakgoorak@gmail.com>
2024-08-20 10:38:01 +0700

    sos

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Pagination({ currentPage, size, totalItems, onChangePage, limitPage }) {
-    const totalPages = Math.ceil(totalItems / size);
+function Pagination({ currentPage, size, limitPage, totalItems, onChangePage }) {
+    const totalPages = totalItems ? Math.ceil(totalItems / size) : 10;
+    const [pCurrentPage, setpCurrentPage] = useState(1);
+    const [pLimitPage, setpLimitPage] = useState(limitPage || 5);
 
-    const getPaginationNumbers = () => {
+    const getPaginationNumbers = (currentPage, totalPages, limitPage) => {
         const pageNumbers = [];
         const startPage = Math.max(2, currentPage - Math.floor(limitPage / 2));
         const endPage = Math.min(totalPages - 1, currentPage + Math.floor(limitPage / 2));
@@ -23,30 +25,55 @@ function Pagination({ currentPage, size, totalItems, onChangePage, limitPage }) 
         return [1, ...pageNumbers, totalPages];
     };
 
-    const prevPage = () => currentPage > 1 && onChangePage(currentPage - 1);
-    const nextPage = () => currentPage < totalPages && onChangePage(currentPage + 1);
+    const prevPage = (onChangePage, currentPage) => {
+        const newPage = currentPage > 1 ? currentPage - 1 : 1;
+        if (onChangePage) {
+            onChangePage(newPage);
+        } else {
+            setpCurrentPage(newPage);
+        }
+    };
+
+    const nextPage = (onChangePage, currentPage, totalPages) => {
+        const newPage = currentPage < totalPages ? currentPage + 1 : totalPages;
+        if (onChangePage) {
+            onChangePage(newPage);
+        } else {
+            setpCurrentPage(newPage);
+        }
+    };
+
+    const paginationNumbers = onChangePage
+        ? getPaginationNumbers(currentPage, totalPages, limitPage)
+        : getPaginationNumbers(pCurrentPage, totalPages, pLimitPage);
 
     return (
         <nav>
             <ul className='pagination'>
-                <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
-                    <button className='page-link' onClick={prevPage} disabled={currentPage === 1}>
+                <li className={`page-item ${(onChangePage ? currentPage : pCurrentPage) === 1 ? 'disabled' : ''}`}>
+                    <button
+                        className='page-link'
+                        onClick={() => prevPage(onChangePage, onChangePage ? currentPage : pCurrentPage)}
+                    >
                         &lt;
                     </button>
                 </li>
-                {getPaginationNumbers().map((num) => (
-                    <li className={`page-item ${currentPage === num ? 'active' : ''}`}>
+                {paginationNumbers.map((num, index) => (
+                    <li key={index} className={`page-item ${(onChangePage ? currentPage : pCurrentPage) === num ? 'active' : ''}`}>
                         <button
                             className='page-link'
-                            onClick={() => num !== '...' && onChangePage(num)}
+                            onClick={() => num !== '...' && (onChangePage ? onChangePage(num) : setpCurrentPage(num))}
                             disabled={num === '...'}
                         >
                             {num}
                         </button>
                     </li>
                 ))}
-                <li className={`page-item ${currentPage === totalPages ? 'disabled' : ''}`}>
-                    <button className='page-link' onClick={nextPage} disabled={currentPage === totalPages}>
+                <li className={`page-item ${(onChangePage ? currentPage : pCurrentPage) === totalPages ? 'disabled' : ''}`}>
+                    <button
+                        className='page-link'
+                        onClick={() => nextPage(onChangePage, onChangePage ? currentPage : pCurrentPage, totalPages)}
+                    >
                         &gt;
                     </button>
                 </li>
